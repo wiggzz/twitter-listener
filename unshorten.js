d5ac5c876ee72add9ac223fc0d3d87548aa1a6a8
Will James <wjames@thoughtworks.com>
2016-03-20 15:47:58 +1100

    Make unshorten have a default user agent.

--- unshorten.js
+++ unshorten.js
@@ -1,10 +1,30 @@
-var request = require('request');
+'use strict';
 
-function unshorten(url, callback) {
+let request = require('request');
+
+let defaultOptions = {
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'
+};
+
+function extractOptions(options) {
+  return Object.assign({}, defaultOptions, options);
+}
+
+function unshorten(url, options, callback) {
+  if (!callback) {
+    callback = options;
+    options = {}
+  }
+
+  options = extractOptions(options);
+
   request({
     method: 'HEAD',
     url,
-    followAllRedirects: true
+    followAllRedirects: true,
+    headers: {
+      'User-Agent': options.userAgent
+    }
   }, (error, response) => {
     if (error) {
       callback(error);
